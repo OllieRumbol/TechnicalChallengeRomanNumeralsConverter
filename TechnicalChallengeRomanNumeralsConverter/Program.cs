4b34b93a4f5068acf6c1c0467f87e005af1bcf95
Oliver Bourne <otgb97@gmail.com>
2023-02-01 14:18:52 +0000

    Creating project and converting to roman numerial

---
+++ TechnicalChallengeRomanNumeralsConverter/Program.cs
@@ -0,0 +1,8 @@
+﻿using TechnicalChallengeRomanNumeralsConverterService;
+
+int numberToConvert = 17;
+ConverterService convertService = new ConverterService();
+string result = convertService.ConvertNumberToRomanNumerial(numberToConvert);
+
+Console.WriteLine(numberToConvert);
+Console.WriteLine(result);
---
+++ TechnicalChallengeRomanNumeralsConverterService/ConverterService.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+
+namespace TechnicalChallengeRomanNumeralsConverterService
+{
+    public class ConverterService
+    {
+        public string ConvertNumberToRomanNumerial(int numberToConvert)
+        {
+            StringBuilder bobTheBuilder = new StringBuilder();
+
+            while (numberToConvert > 0)
+            {
+                if (numberToConvert - 100 >= 0)
+                {
+                    bobTheBuilder.Append("C");
+                    numberToConvert = numberToConvert - 100;
+                }
+                else if (numberToConvert - 90 >= 0)
+                {
+                    bobTheBuilder.Append("XC");
+                    numberToConvert = numberToConvert - 90;
+                }
+                else if (numberToConvert - 50 >= 0)
+                {
+                    bobTheBuilder.Append("L");
+                    numberToConvert = numberToConvert - 50;
+                }
+                else if (numberToConvert - 40 >= 0)
+                {
+                    bobTheBuilder.Append("XL");
+                    numberToConvert = numberToConvert - 40;
+                }
+                else if (numberToConvert - 10 >= 0)
+                {
+                    bobTheBuilder.Append("X");
+                    numberToConvert = numberToConvert - 10;
+                }
+                else if (numberToConvert - 9 >= 0)
+                {
+                    bobTheBuilder.Append("IX");
+                    numberToConvert = numberToConvert - 9;
+                }
+                else if (numberToConvert - 5 >= 0)
+                {
+                    bobTheBuilder.Append("V");
+                    numberToConvert = numberToConvert - 5;
+                }
+                else if (numberToConvert - 4 >= 0)
+                {
+                    bobTheBuilder.Append("IV");
+                    numberToConvert = numberToConvert - 4;
+                }
+                else if (numberToConvert - 1 >= -0)
+                {
+                    bobTheBuilder.Append("I");
+                    numberToConvert = numberToConvert - 1;
+                }
+            }
+
+            return bobTheBuilder.ToString();
+        }
+    }
+}
